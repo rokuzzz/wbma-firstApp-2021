a05b7cdca79fe4948f44f7f7fec8650d62081c7a
Roman Kuzero <rombeldan@gmail.com>
2021-01-24 23:17:31 +0300

    forms final commit

--- views/Login.js
+++ views/Login.js
@@ -1,27 +1,29 @@
 import React, {useContext, useEffect} from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import {StyleSheet, View, Text, KeyboardAvoidingView} from 'react-native';
 import PropTypes from 'prop-types';
 import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useLogin} from '../hooks/ApiHooks';
+import LoginForm from '../components/LoginForm';
+import RegisterForm from '../components/RegisterForm';
 
 const Login = ({navigation}) => {
-  const [isLoggedIn, setIsLoggedIn] = useContext(MainContext);
-  console.log('ili', isLoggedIn);
-  const logIn = async () => {
-    setIsLoggedIn(true);
-    await AsyncStorage.setItem('userToken', 'abc');
-    if (isLoggedIn) {
-      // this is to make sure isLoggedIn has changed, will be removed later
-      navigation.navigate('Home');
-    }
-  };
+  const {isLoggedIn, setIsLoggedIn, setUser} = useContext(MainContext);
+  console.log('isLoggedIn?', isLoggedIn);
+  const {checkToken} = useLogin();
 
   const getToken = async () => {
     const userToken = await AsyncStorage.getItem('userToken');
     console.log('token', userToken);
-    if (userToken === 'abc') {
-      setIsLoggedIn(true);
-      navigation.navigate('Home');
+    if (userToken) {
+      try {
+        const userData = await checkToken(userToken);
+        setIsLoggedIn(true);
+        setUser(userData);
+        navigation.navigate('Home');
+      } catch (error) {
+        console.log('token check failed', error.message);
+      }
     }
   };
   useEffect(() => {
@@ -29,10 +31,14 @@ const Login = ({navigation}) => {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-      <Button title="Sign in!" onPress={logIn} />
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.container}>
+        <Text>Login</Text>
+        <LoginForm navigation={navigation} />
+        <Text>Register</Text>
+        <RegisterForm navigation={navigation} />
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
